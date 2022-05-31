4a74ea5e36302739e82aaed56efbe57d564d0047
NEKETSKY <nikitaverbovikov@gmail.com>
2022-05-31 09:17:37 +0300

    go mod

--- student_test.go
+++ student_test.go
@@ -1,5 +1,7 @@
 package coverage
 
+import "testing"
+
 /*func TestConcatenator(t *testing.T) {
 	want := "GolangUnited"
 	_, got := concatenator("Golang", "United")
@@ -7,3 +9,5 @@ package coverage
 		t.Errorf("Error: want and got do not match\nwant: %v, got: %v\n", want, got)
 	}
 }*/
+
+func TestConcatenator(t *testing.T) {}
