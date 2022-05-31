4b607ba2b6dec56796c752fcfabbde832cbc7462
NEKETSKY <nikitaverbovikov@gmail.com>
2022-05-31 09:38:13 +0300

    half-coverage test

--- student_test.go
+++ student_test.go
@@ -11,5 +11,9 @@ import "testing"
 }*/
 
 func TestConcatenator(t *testing.T) {
-
+	want := "GolangUnited"
+	_, got := concatenator("Golang", "United")
+	if got != want {
+		t.Errorf("Error: want and got do not match\nwant: %v, got: %v\n", want, got)
+	}
 }
