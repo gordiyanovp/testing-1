02e0993e0881eb94f0943d3e36cc8ce2ff56ed4c
NEKETSKY <nikitaverbovikov@gmail.com>
2022-05-31 09:50:03 +0300

    use build tags

--- student_test.go
+++ student_test.go
@@ -1,14 +1,8 @@
+//go:build student_tests
+
 package coverage
 
 import "testing"
-
-/*func TestConcatenator(t *testing.T) {
-	want := "GolangUnited"
-	_, got := concatenator("Golang", "United")
-	if got != want {
-		t.Errorf("Error: want and got do not match\nwant: %v, got: %v\n", want, got)
-	}
-}*/
 
 func TestConcatenator(t *testing.T) {
 	want := "GolangUnited"
